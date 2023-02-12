a5e5cafbf7a27d88f649fdab8225c292d37255ec
HuzhynVadim <huzhynvadim@gmail.com>
2023-02-13 01:17:15 +0200

    local

--- Core.lua
+++ Core.lua
@@ -1,14 +1,18 @@
 CLM = LibStub("AceAddon-3.0"):NewAddon("CLM")
 
+local function checkTable(table)
+	return rawequal(next(table), nil)
+end
+
 function CLM:OnInitialize()
 	CLM.AceAddonName = "ConsulLootMaster"
 	CLM.AddonNameAndVersion = "ConsulLootMaster 0.0.4"
-	CLM:initConfig()
-	CLM:addMapIcon()
-	if not #CLMWishlistsType == 0 and not #CLMNickname == 0 and not #CLMWishlists == 0 then
+	if not checkTable(CLMNickname) and not checkTable(CLMWishlists) then
+		CLM:initConfig()
+		CLM:addMapIcon()
 		CLM:initWishlists()
 	end
-	if not #CLMItems == 0 then
+	if not checkTable(CLMItems) then
 		CLM:initBisTooltip()
 	end
 end
