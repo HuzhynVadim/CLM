42f7e77b3ddedb1dcfafa9cd741a61675379654e
HuzhynVadim <huzhynvadim@gmail.com>
2023-03-07 19:35:39 +0200

    ref

--- Core.lua
+++ Core.lua
@@ -4,7 +4,7 @@ function CLM:OnInitialize()
 	CLM.AceAddonName = "ConsulLootMaster"
 	CLM.Version = 1.0
 	CLM.AddonNameAndVersion = "ConsulLootMaster 1.0"
-	if not CLM:checkTable(CLMNickname) and not CLM:checkTable(CLMWishlists) then
+	if not CLM:checkTable(CLMWishlistsType) and not CLM:checkTable(CLMNickname) and not CLM:checkTable(CLMWishlists) then
 		CLM:initConfig()
 		CLM:addMapIcon()
 		CLM:initWishlists()
